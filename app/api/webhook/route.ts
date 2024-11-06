e131769a02d316cd95ba4b0e816877947140d3fd
Kareem <kareemadel10110@gmail.com>
2024-11-06 20:31:26 +0200

    🔥🔥

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -31,7 +31,7 @@ export async function POST(req: NextRequest) {
         const charge = data as Stripe.Charge;
         userEmail = charge.billing_details.email;
         planId = charge.metadata.product_id || charge.payment_intent;
-        amount = charge.amount;
+        amount = charge.amount / 100;
       }
 
       if (!userEmail || !planId || !amount) {
