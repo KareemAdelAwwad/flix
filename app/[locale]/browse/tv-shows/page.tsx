f9181e41693eb1e1a15783e6bca475d559812310
ibrahim wael <ibrahimwael809@gmail.com>
2024-11-14 03:04:54 +0200

    edit the tv shows

--- app/[locale]/browse/tv-shows/page.tsx
+++ app/[locale]/browse/tv-shows/page.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import TVShows from "@/components/TV-shows"
 export default function page() {
   return (
     <section className='container'>
-      TV Showes
+   <TVShows/>
     </section>
   )
 }
